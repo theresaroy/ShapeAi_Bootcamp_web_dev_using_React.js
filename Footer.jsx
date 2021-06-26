5e6d6388790b0986bca9ff0161662dd479c91d32
theresaroy <85453096+theresaroy@users.noreply.github.com>
2021-06-26 22:31:36 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Footer() { 
+  return  (
+   <footer>
+     <p>Copyright by ShapeAi @ {new Date().getFullYear()}</p>
+   </footer>
+  );
+  
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Info() { 
+  return  (
+    <div className="note">
+     <h1>Javascript and React.js</h1>
+     <p>This was an amazing bootcamp taken up by Shaurya Sinha.We covered everything from Scratch including Javascript React.js,HTML.</p>
+   </div>
+  );
+  
+}
+
+export default Info;
